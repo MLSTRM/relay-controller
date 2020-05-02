e0f39e1e16077df4359441d113bcc414ba1abdcc
MLSTRM <chris_nitro@hotmail.co.uk>
2020-05-02 19:58:07 +0100

    Fix info cycling

--- ffrelaytoolv1/TeamControl.cs
+++ ffrelaytoolv1/TeamControl.cs
@@ -211,7 +211,7 @@ namespace ffrelaytoolv1
                 if (targetTab > -1)
                 {
                     teamTabGroup.SelectedIndex = Util.clamp(targetTab, teamTabGroup.TabCount, 0);
-                } else if (teamTabGroup.SelectedIndex == teamTabGroup.TabCount)
+                } else if (teamTabGroup.SelectedIndex == teamTabGroup.TabCount - 1)
                 { teamTabGroup.SelectedIndex = 0; }
                 else { teamTabGroup.SelectedIndex++; }
             }
